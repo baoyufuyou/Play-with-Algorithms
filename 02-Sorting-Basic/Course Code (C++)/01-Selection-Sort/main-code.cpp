0cc3c0b981208f09b151605d36b884a52fdb36dc
Lei Zhang <lz@ar-lx0138.ar.int>
2020-05-29 17:38:57 +0200

    daily update

---
+++ 02-Sorting-Basic/Course Code (C++)/01-Selection-Sort/main-code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+//The algorithms library defines functions for a variety of purposes 
+//(e.g. searching, sorting, counting, manipulating) that operate on 
+//ranges of elements. Note that a range is defined as [first, last) 
+//where last refers to the element past the last element to inspect or modify.
+using namespace std;
+void selectionSort(int arr[], int n){
+    for(int i = 0; i < n ; i++){
+        int minIndex = i;
+        for(int j = i+1; j<n; j++){ // 对于每一个初始位置，用后面的进行比较， 如果需要变幻顺序，进行swap替换
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main(){
+    int a[10] = {}
+}
